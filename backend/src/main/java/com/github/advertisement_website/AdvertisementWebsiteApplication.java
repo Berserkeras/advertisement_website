066fad5712849905c51db1f52493f63c8e246ba1
Berserkeras <emilir96@protonmail.com>
2023-02-10 00:58:18 +0200

    backend project skeleton

---
+++ backend/src/main/java/com/github/advertisement_website/AdvertisementWebsiteApplication.java
@@ -0,0 +1,14 @@
+package com.github.advertisement_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertisementWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertisementWebsiteApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ backend/src/test/java/com/github/advertisement_website/AdvertisementWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.advertisement_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertisementWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
