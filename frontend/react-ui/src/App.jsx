d308548b59ce84925ff47eda0221439adddb752e
Berserkeras <emilir96@protonmail.com>
2023-02-10 19:57:02 +0200

    moved frontend from .js to .jsx, added routes to  App.jsx

---
+++ frontend/react-ui/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import CreateAd from './pages/CreateAd'
+import Landing from "./pages/Landing";
+import Error from './pages/ErrorPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+function App () {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="register" element={<CreateAd />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
+            <ToastContainer />
+        </BrowserRouter>
+    )
+}
+
+export default App
