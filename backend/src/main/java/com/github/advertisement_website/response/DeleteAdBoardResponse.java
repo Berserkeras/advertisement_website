388476e72e19b3c95695270c213e11bc9e432bb5
Berserkeras <emilir96@protonmail.com>
2023-03-06 22:47:19 +0200

    added error catchers, created custom catchers

--- backend/src/main/java/com/github/advertisement_website/response/DeleteAdBoardResponse.java
+++ backend/src/main/java/com/github/advertisement_website/response/DeleteAdBoardResponse.java
@@ -1,28 +1,10 @@
 package com.github.advertisement_website.response;
 
+import org.springframework.http.HttpStatus;
+
 public class DeleteAdBoardResponse {
-
-    private MessageType messageType;
-    private String message;
-    public DeleteAdBoardResponse(MessageType messageType, String message) {
-        this.messageType = messageType;
-        this.message = message;
+    public DeleteAdBoardResponse(HttpStatus messageType, String message) {
     }
 
-    public MessageType getMessageType() {
-        return messageType;
-    }
-
-    public void setMessageType(MessageType messageType) {
-        this.messageType = messageType;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 
 }
